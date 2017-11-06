cd76f917a304687d9dfb027f27f69c91449aad3b
Bernardo Cordeiro <bernardo.f.cordeiro@tecnico.ulisboa.pt>
2017-11-06 11:46:16 +0000

    Repo start

---
+++ src/main/java/pt/ulisboa/tecnico/ssof/App.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.ssof;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
