bea0be027da3c4a776fa5fb336f7fb7c99679a23
Bernardo Cordeiro <bernardo.f.cordeiro@tecnico.ulisboa.pt>
2017-11-13 11:09:06 +0000

    run

--- src/main/java/pt/ulisboa/tecnico/ssof/App.java
+++ src/main/java/pt/ulisboa/tecnico/ssof/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Printing arguments:" );
+        for(String arg: args)
+            System.out.println(arg);
     }
 }
