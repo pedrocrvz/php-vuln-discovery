a87520f9e157967a05ddde5453fdf7c3d77eb190
Bernardo Cordeiro <bernardo.f.cordeiro@tecnico.ulisboa.pt>
2017-11-13 11:21:09 +0000

    analyser

---
+++ src/main/java/pt/ulisboa/tecnico/ssof/Analyser.java
@@ -0,0 +1,10 @@
+package pt.ulisboa.tecnico.ssof;
+
+import java.io.File;
+
+public class Analyser {
+    private File file;
+    public Analyser(File file){
+        this.file = file;
+    }
+}
--- src/main/java/pt/ulisboa/tecnico/ssof/App.java
+++ src/main/java/pt/ulisboa/tecnico/ssof/App.java
@@ -1,15 +1,22 @@
 package pt.ulisboa.tecnico.ssof;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Printing arguments:" );
-        for(String arg: args)
-            System.out.println(arg);
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) {
+        if(args.length < 1){
+            System.err.println("Argument expected");
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+        File file = new File(filePath);
+
+        if(!file.exists() || file.isDirectory()){
+            System.err.println("Argument is expected to be a valid file path");
+            System.exit(1);
+        }
+
+        Analyser analyser = new Analyser(file);
     }
 }
